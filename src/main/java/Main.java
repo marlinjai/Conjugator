113b9ecc3d97ebfa9207af274d7b9bdb69ffc439
Ennoying <72946645+Ennoying@users.noreply.github.com>
2023-01-09 16:06:06 +0100

    noUpdate

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ import java.lang.reflect.AccessibleObject;
 public class Main {
 
     public static void main(String[] args) {
-
+        
         //User testUser1 = new User("Anton","Zimmermann", "1234","A.Zimmermann@Zmail.de");
 
         //AppCtrl.logIn(testUser1,"A.Zimmermann@Zmail.de", "1234", Database.Userlist);
