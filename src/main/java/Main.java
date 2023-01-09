2f980715c82b95d8ab560abaede57b94ebb10257
Ennoying <72946645+Ennoying@users.noreply.github.com>
2023-01-09 22:14:21 +0100

    merge conflict

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,12 +9,7 @@ import java.lang.reflect.AccessibleObject;
 public class Main {
 
     public static void main(String[] args) {
-        
-        //User testUser1 = new User("Anton","Zimmermann", "1234","A.Zimmermann@Zmail.de");
 
-<<<<<<< HEAD
-        //AppCtrl.logIn(testUser1,"A.Zimmermann@Zmail.de", "1234", Database.Userlist);
-=======
         User testUser1 = new User("Anton","Zimmermann", "1234","A.Zimmermann@Zmail.de");
 
         AppCtrl.logIn(testUser1,"A.Zimmermann@Zmail.de", "1234", Database.Userlist);
@@ -87,7 +82,6 @@ public class Main {
             }
         }
 =======
->>>>>>> master
 >>>>>>> master
     }
 
